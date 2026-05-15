f56ae6432ab2d5360150929626794a23c5f21250
MannPatel15012009 <mannpatel15012009314159@gmail.com>
2026-05-15 23:53:37 +0530

    Implement message handling and calculation feature
    Added a calculate function to process messages and respond with evaluated expressions.

--- TestBot3.py
+++ TestBot3.py
@@ -1,5 +1,18 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes         
+from telegram.ext import Application, ContextTypes, MessageHandler, filters 
+import os
+from sympy import sympify
+
 TOKEN = os.environ.get('TOKEN3')
-async def main():
-    print("Bot is running...")
+
+async def calculate(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # print(f"Received message: {update.message}")
+    await update.message.reply_text(str(sympify(update.message.text)))
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(MessageHandler( filters.TEXT & ~filters.COMMAND, calculate))
+
+print("Bot is running...")
+
+app.run_polling()   
